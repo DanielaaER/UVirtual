db08be97da12bfe5b0ba5fdab446e4b904e7608f
Daniela Espinosa <daniela.espinosa.3103@gmail.com>
2023-03-05 13:44:53 -0600

    cambios de carpetas

--- api/app.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from routes.login import user
-app= FastAPI()
-
-app.include_router(user)
--- api/config/db.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine, MetaData
-import os
-from dotenv import load_dotenv
-
-create_engine(f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
